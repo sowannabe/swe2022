e51acdbbd780f02328158204046530c5e9c94b3d
sowannabe <riverpool365@naver.com>
2017-09-23 23:08:02 +0900

    test ver

---
+++ src/lesson4/test0.java
@@ -0,0 +1,12 @@
+package lesson4;
+
+public class test0 {
+    public static int add(int a,int b){
+        int c=a+b;
+        return c;
+    }
+    public static void main(String[] arg){
+        int b=add(3,4);
+        System.out.println(b);
+    }
+}
